1b258b452435be95c9984a0f6aee26085a555d14
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-02 20:08:23 +0300

    Add new img and script.js file

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+
+const slider = tns({
+    container: '.community__img',
+    items: 2,
+    slideBy: 1,
+    speed: 800,
+    autoplay: false,
+    controls: false,
+    gutter: 24,
+    nav: false
+});
+
+document.querySelector('.button_community-prev').addEventListener ('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.button_community-next').addEventListener ('click', function () {
+    slider.goTo('next');
+});
+
+const slr = tns({
+    container: '.design__carousel',
+    items: 1,
+    slideBy: 1,
+    speed: 800,
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.button_design-prev').addEventListener ('click', function () {
+    slr.goTo('prev');
+});
+
+document.querySelector('.button_design-next').addEventListener ('click', function () {
+    slr.goTo('next');
+});
